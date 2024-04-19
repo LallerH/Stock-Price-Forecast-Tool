9cc965ce2ead3df6324bf7a1f9c21378fc99a521
Lajos Hegedus <hegedus_lajos@hotmail.com>
2024-04-19 22:36:02 +0200

    fixing gitignore file and file structure, addig readme file

--- stock_price_forecast.py
+++ stock_price_forecast.py
@@ -1,5 +1,5 @@
 import pandas as pd
-from data_supply import initial_download_from_yahoofin, add_indicator
+from utils import initial_download_from_yahoofin, add_indicator
 
 if __name__ == '__main__':
     stock_df = initial_download_from_yahoofin(period='1y')
